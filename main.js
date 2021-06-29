c4327d4a513f7b7f97e2e7ea32e09e070352d43d
krishlala <71400038+krishlala@users.noreply.github.com>
2021-06-29 15:59:21 -0400

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function take_snapshot(){
 
 console.log("ml5 version: ", ml5.version);
 
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/RfuLKcdps/model.json", modelLoaded);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/rrsB6kMMQ/model.json", modelLoaded);
 
 function modelLoaded(){
     console.log("Model Loaded!");
